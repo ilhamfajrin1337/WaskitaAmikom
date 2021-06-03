0b5c3a0b620b6c9ef99dc1a7c44c244258f2cebe
ilhamfajrin1337 <ilham.14@student.amikom.ac.id>
2021-06-03 20:00:28 +0700

    CUMA KOMEN KOK

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="card card-primary">
   <div class="card-header"><h4>Login</h4></div>
 
-{{-- MENCOBA UBAH DARI LAPTOP ILMA --}}
+{{-- MENCOBA UBAH DARI LAPTOP ILMA yang kedua --}}
   <div class="card-body">
     <form method="POST" action="{{ route('login') }}">
         @csrf
